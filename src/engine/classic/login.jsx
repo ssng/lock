c1a3c7f2d90d22dfd34dc89add646d27a7f62e00
Swee Sng <ssng@sharpsec.com>
2021-07-15 06:24:56 +0000

    Mimic identifier-first login flow

--- src/engine/classic/login.jsx
+++ src/engine/classic/login.jsx
@@ -18,6 +18,7 @@ import LoginSignUpTabs from '../../connection/database/login_sign_up_tabs';
 import * as l from '../../core/index';
 import { logIn as enterpriseLogIn, startHRD } from '../../connection/enterprise/actions';
 import {
+  matchConnection, // SSH
   defaultEnterpriseConnection,
   findADConnectionWithoutDomain,
   isHRDDomain
@@ -25,12 +26,24 @@ import {
 import SingleSignOnNotice from '../../connection/enterprise/single_sign_on_notice';
 import { hasOnlyClassicConnections, isSSOEnabled } from '../classic';
 import * as i18n from '../../i18n';
+// SSH
+import { authButtonsTheme } from '../../connection/social/index';
+import QuickAuthPane from '../../ui/pane/quick_auth_pane';
+import { password } from '../../field/index';
 
 function shouldRenderTabs(m) {
   if (isSSOEnabled(m)) return false;
   if (l.hasSomeConnections(m, 'database')) return hasScreen(m, 'signUp');
   if (l.hasSomeConnections(m, 'social') && hasInitialScreen(m, 'signUp'))
     return hasScreen(m, 'signUp');
+}
+
+// SSH
+// TODO: handle this from CSS
+function icon(strategy) {
+  if (strategy === 'google-apps') return strategy;
+  if (~['adfs', 'office365', 'waad'].indexOf(strategy)) return 'windows';
+  return 'auth0';
 }
 
 const Component = ({ i18n, model }) => {
@@ -57,14 +70,22 @@ const Component = ({ i18n, model }) => {
     />
   );
 
+  // SSH
   const showPassword =
-    !sso && (l.hasSomeConnections(model, 'database') || !!findADConnectionWithoutDomain(model));
+    l.hasSomeConnections(model, 'database') &&
+    !l.hasSomeConnections(model, 'enterprise') && !l.hasSomeConnections(model, 'social');
+  // SSH   (l.hasSomeConnections(model, 'database') || !!findADConnectionWithoutDomain(model));
+  // ORIGINAL    !sso && (l.hasSomeConnections(model, 'database') || !!findADConnectionWithoutDomain(model));
 
   const showForgotPasswordLink = showPassword && l.hasSomeConnections(model, 'database');
 
   const loginInstructionsKey = social
     ? 'databaseEnterpriseAlternativeLoginInstructions'
     : 'databaseEnterpriseLoginInstructions';
+
+  // SSH replace 'or' with header for login panel
+  //const loginPanelHeader = l.hasSomeConnections(model, 'database') ? 'Sharp Synappx Account' : social ? i18n.html(loginInstructionsKey) : 'Sharp Synappx Login';
+  const loginPanelHeader = 'Sharp Synappx Account';
 
   const usernameInputPlaceholderKey =
     databaseUsernameStyle(model) === 'any' || l.countConnections(model, 'enterprise') > 1
@@ -80,7 +101,8 @@ const Component = ({ i18n, model }) => {
       emailInputPlaceholder={i18n.str('emailInputPlaceholder')}
       forgotPasswordAction={i18n.str('forgotPasswordAction')}
       i18n={i18n}
-      instructions={i18n.html(loginInstructionsKey)}
+      //instructions={i18n.html(loginInstructionsKey)}
+      instructions={loginPanelHeader}
       lock={model}
       passwordInputPlaceholder={i18n.str('passwordInputPlaceholder')}
       showForgotPasswordLink={showForgotPasswordLink}
@@ -94,6 +116,139 @@ const Component = ({ i18n, model }) => {
 
   const separator = social && login && <PaneSeparator />;
 
+  // SSH
+  const separator2 = sso && login && <PaneSeparator />;
+  //const separator2 = sso && l.hasSomeConnections(model, 'database') && login && <PaneSeparator />;
+  const ssoConnection = sso && matchConnection(model, databaseUsernameValue(model));
+  const theme = authButtonsTheme(model);
+  const buttonTheme = theme && ssoConnection && theme.get(ssoConnection.get('name'));
+  const connectionDomain = ssoConnection && ssoConnection.getIn(['domains', 0]);
+  const connectionStrategy =
+    ssoConnection &&
+    (ssoConnection.get('strategy') === 'waad' ? 'Microsoft 365' : 'Google Workspace');
+  const buttonLabel =
+    (buttonTheme && buttonTheme.get('displayName')) ||
+    (connectionDomain && i18n.str('loginAtLabel', connectionStrategy));
+  const primaryColor = buttonTheme && buttonTheme.get('primaryColor');
+  const foregroundColor = buttonTheme && buttonTheme.get('foregroundColor');
+  const buttonIcon = buttonTheme && buttonTheme.get('icon');
+
+  const quickAuth = sso && (
+    <QuickAuthPane
+      buttonLabel={buttonLabel}
+      buttonClickHandler={e => enterpriseLogIn(1)}
+      //header={headerText}
+      buttonIcon={buttonIcon}
+      primaryColor={primaryColor}
+      foregroundColor={foregroundColor}
+      strategy={icon(ssoConnection.get('strategy'))}
+    />
+  );
+
+  const dbConnections = l.connections(model, 'database'); // support multiple DB connections
+  // first DB connection
+  const dbSeparator = (l.hasSomeConnections(model, 'enterprise') ||
+    l.hasSomeConnections(model, 'social')) &&
+    l.hasSomeConnections(model, 'database') &&
+    login && <PaneSeparator />;
+  const dbConnection = l.hasSomeConnections(model, 'database') && dbConnections.get(0); // get first DB connection
+  const dbButtonTheme = theme && dbConnection && theme.get(dbConnection.get('name'));
+  const dbButtonLabel =
+    (dbButtonTheme && dbButtonTheme.get('displayName')) ||
+    (dbConnection && dbConnection.get('name'));
+  const dbPrimaryColor = dbButtonTheme && dbButtonTheme.get('primaryColor');
+  const dbForegroundColor = dbButtonTheme && dbButtonTheme.get('foregroundColor');
+  const dbButtonIcon = dbButtonTheme && dbButtonTheme.get('icon');
+  let dbUrlPart = '';
+  for (const param in config.extraParams) {
+    if (!param.startsWith('_') && param !== 'state') {
+      if (dbUrlPart != '') {
+        dbUrlPart += '&';
+      }
+      dbUrlPart += param;
+      dbUrlPart += '=';
+      dbUrlPart += encodeURIComponent(config.extraParams[param]);
+    }
+  }
+  const dbUrlLastPart =
+    dbConnection &&
+    '&redirect_uri=' +
+      encodeURIComponent(config.callbackURL) +
+      '&client_id=' +
+      encodeURIComponent(config.clientID) +
+      '&connection=' +
+      encodeURIComponent(dbConnection.get('name')) +
+      '&login_hint=' +
+      encodeURIComponent(databaseUsernameValue(model));
+  const dbClickUrl = config.authorizationServer.issuer + 'authorize?' + dbUrlPart + dbUrlLastPart;
+
+  const dbAuth = (l.hasSomeConnections(model, 'enterprise') ||
+    l.hasSomeConnections(model, 'social')) &&
+    dbConnection && (
+      <QuickAuthPane
+        buttonLabel={dbButtonLabel}
+        buttonClickHandler={e => (location.href = dbClickUrl)}
+        //header={headerText}
+        buttonIcon={dbButtonIcon}
+        primaryColor={dbPrimaryColor}
+        foregroundColor={dbForegroundColor}
+        strategy={dbConnection.get('strategy')}
+      />
+    );
+
+  // second DB connection
+  const dbSeparator2 = (l.hasSomeConnections(model, 'enterprise') ||
+    l.hasSomeConnections(model, 'social')) &&
+    l.hasSomeConnections(model, 'database') &&
+    login &&
+    dbConnections.count() > 1 && <PaneSeparator />;
+  const dbConnection2 =
+    l.hasSomeConnections(model, 'database') && dbConnections.count() > 1 && dbConnections.get(1); // get second DB connection
+  const dbButtonTheme2 = theme && dbConnection2 && theme.get(dbConnection2.get('name'));
+  const dbButtonLabel2 =
+    (dbButtonTheme2 && dbButtonTheme2.get('displayName')) ||
+    (dbConnection2 && dbConnection2.get('name'));
+  const dbPrimaryColor2 = dbButtonTheme2 && dbButtonTheme2.get('primaryColor');
+  const dbForegroundColor2 = dbButtonTheme2 && dbButtonTheme2.get('foregroundColor');
+  const dbButtonIcon2 = dbButtonTheme2 && dbButtonTheme2.get('icon');
+  let dbUrlPart2 = '';
+  for (const param in config.extraParams) {
+    if (!param.startsWith('_') && param !== 'state') {
+      if (dbUrlPart2 != '') {
+        dbUrlPart2 += '&';
+      }
+      dbUrlPart2 += param;
+      dbUrlPart2 += '=';
+      dbUrlPart2 += encodeURIComponent(config.extraParams[param]);
+    }
+  }
+  const dbUrlLastPart2 =
+    dbConnection2 &&
+    '&redirect_uri=' +
+      encodeURIComponent(config.callbackURL) +
+      '&client_id=' +
+      encodeURIComponent(config.clientID) +
+      '&connection=' +
+      encodeURIComponent(dbConnection2.get('name')) +
+      '&login_hint=' +
+      encodeURIComponent(databaseUsernameValue(model));
+  const dbClickUrl2 =
+    config.authorizationServer.issuer + 'authorize?' + dbUrlPart2 + dbUrlLastPart2;
+
+  const dbAuth2 = (l.hasSomeConnections(model, 'enterprise') ||
+    l.hasSomeConnections(model, 'social')) &&
+    dbConnection2 && (
+      <QuickAuthPane
+        buttonLabel={dbButtonLabel2}
+        buttonClickHandler={e => (location.href = dbClickUrl2)}
+        //header={headerText}
+        buttonIcon={dbButtonIcon2}
+        primaryColor={dbPrimaryColor2}
+        foregroundColor={dbForegroundColor2}
+        strategy={dbConnection2.get('strategy')}
+      />
+    );
+
   return (
     <div>
       {ssoNotice}
@@ -101,6 +256,12 @@ const Component = ({ i18n, model }) => {
       <div>
         {social}
         {separator}
+        {dbAuth}
+        {dbSeparator}
+        {dbAuth2}
+        {dbSeparator2}
+        {quickAuth}
+        {separator2}
         {login}
       </div>
     </div>
@@ -129,13 +290,20 @@ export default class Login extends Screen {
     // requires username/password and there is no enterprise with domain
     // that matches with the email domain entered for HRD
     return (
-      !l.hasSomeConnections(m, 'database') && // no database connection
+      // SSH disable login button until password field is not empty
+      (l.hasSomeConnections(m, 'database') && password(m) === '') ||
+      (!l.hasSomeConnections(m, 'database') && // no database connection
       !findADConnectionWithoutDomain(m) && // no enterprise without domain
-      !isSSOEnabled(m)
+        !isSSOEnabled(m))
     ); // no matching domain
   }
 
   submitHandler(model) {
+    // SSH hide login button on first page
+    if (l.hasSomeConnections(model, 'enterprise') || l.hasSomeConnections(model, 'social')) {
+      return null;
+    }
+
     if (hasOnlyClassicConnections(model, 'social')) {
       return null;
     }
@@ -149,7 +317,9 @@ export default class Login extends Screen {
       databaseConnection(model) &&
       (defaultDatabaseConnection(model) || !defaultEnterpriseConnection(model));
 
-    return useDatabaseConnection ? databaseLogIn : enterpriseLogIn;
+    // SSH
+    return l.hasSomeConnections(model, 'database') ? databaseLogIn : enterpriseLogIn;
+    //    return useDatabaseConnection ? databaseLogIn : enterpriseLogIn;
   }
 
   render() {
